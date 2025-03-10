fccd1c3c69d98b1ea980e16522241ec138a067d7
jayan-maria-dsouza <jayanmariadsouza@gmail.com>
2025-03-10 20:03:20 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import Chatbot from "./Chatbot";
+
+function App() {
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>MIX Chatbot</h2>
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
